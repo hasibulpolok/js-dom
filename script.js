3c957b98ef9ba488e96fedbb5d7fbc2e8d277157
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-02-09 12:48:44 +0600

    set text and clear text added

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
 
-      const heading = document.createElement("h1");
-      const para =document.createElement("p");
-      const headingText = document.createTextNode("Big Head!");
-      const paraText = document.createTextNode("Para Text!");
-      heading.appendChild(headingText);
-      para.appendChild(paraText);
-      document.body.appendChild(heading);
-      document.body.appendChild(para)
+const heading = document.createElement("h1");
+const para = document.createElement("p");
+const headingText = document.createTextNode("Big Head!");
+const paraText = document.createTextNode("Para Text!");
+heading.appendChild(headingText);
+para.appendChild(paraText);
+document.body.appendChild(heading);
+document.body.appendChild(para);
+
+const story = document.body.querySelector(".story");
+
+const setText = document.body.querySelector("#set-text");
+setText.addEventListener("click", () => {
+    story.textContent = "It was a dark and stormy night...";
+});
+
+const clearText = document.body.querySelector("#clear-text");
+clearText.addEventListener("click", () => {
+    story.textContent = "";
+});
+
+
